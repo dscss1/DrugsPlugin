6c3018570e75b5be5778390748952bec226babe3
dscss2 <euser6256@gmail.com>
2024-10-03 15:21:19 +0300

    initial commit

---
+++ src/main/kotlin/ua/dscss2/drugsPlugin/DrugsPlugin.kt
@@ -0,0 +1,14 @@
+package ua.dscss2.drugsPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DrugsPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
